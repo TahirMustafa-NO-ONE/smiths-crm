d13412c84b027e45759d67491e351eb99eb65f63
TahirMustafa-NO-ONE <tahirmustafa161123@gmail.com>
2026-02-26 10:53:41 +0500

    fixed double navbar issue

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,10 @@
-import Layout from "../components/layout/Layout";
 import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <Component {...pageProps} />
     </SessionProvider>
   );
 }
